23490e89043e45a3111466725f4ce1efb8408f79
Joan <setoba1192@hotmail.com>
2023-03-31 00:32:37 -0500

    constraints, hvurl

--- src/main/java/com/softcaribbean/rh/domain/model/empleado/Empleado.java
+++ src/main/java/com/softcaribbean/rh/domain/model/empleado/Empleado.java
@@ -20,27 +20,33 @@ public class Empleado implements Serializable {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "persona_id")
+    @JoinColumn(name = "persona_id", nullable = false)
     private Persona persona;
 
-    @Column(name = "fecha_contratacion")
+    @Column(name = "fecha_contratacion", nullable = false)
     private LocalDate fechaContratacion;
 
-    @Column(name = "email_corporativo")
+    @Column(name = "email_corporativo", nullable = false)
     private String emailCorporativo;
 
-    @Column(name = "telefono_corporativo")
+    @Column(name = "telefono_corporativo", nullable = false)
     private String telefono_corporativo;
 
-    @Column(name = "direccion_trabajo")
+    @Column(name = "direccion_trabajo", nullable = false)
     private String direccion_trabajo;
 
     @ManyToOne
-    @JoinColumn(name = "ciudad_id")
+    @JoinColumn(name = "ciudad_id", nullable = false)
     private Ciudad ciudad_trabajo;
 
+    /**
+     * Url del documento de la Hoja de Vida
+     */
+    @Column(name = "hoja_vida_url")
+    private String hojaVidaURL;
+
     @ManyToOne
-    @JoinColumn(name = "estado_id")
+    @JoinColumn(name = "estado_id", nullable = false)
     private EntidadEstado estado;
 
     @Column(name = "eliminado", columnDefinition = "boolean default false")
