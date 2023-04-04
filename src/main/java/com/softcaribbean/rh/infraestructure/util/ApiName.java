4d5d9a01fb50c53b0f90bef55bf314c810987b99
Joan <setoba1192@hotmail.com>
2023-04-04 00:38:12 -0500

    agrega constante para manejo de api

---
+++ src/main/java/com/softcaribbean/rh/infraestructure/util/ApiName.java
@@ -0,0 +1,8 @@
+package com.softcaribbean.rh.infraestructure.util;
+
+public final class ApiName {
+
+    public final class AdministracionRecursos {
+        public static final String ADMIN_RESOURCES = "/admin/resources";
+    }
+}
